6c12cf0c5485db377c96dbcd3bf790a12e55d5b5
Pranav Anne <pranav.anne21@gmail.com>
2024-11-24 15:29:59 -0500

    Used context hooks to refactor code.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-import Product from "./components/Product.jsx"
+import Product from "./components/Product.jsx";
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
-    items: [],
+    items: []
   });
 
   function handleAddItemToCart(id) {
@@ -66,20 +67,19 @@ function App() {
     });
   }
 
+  const ctxValue = {items:shoppingCart.items, addItemToCart: handleAddItemToCart, updateCart: handleUpdateCartItemQuantity}
+
   return (
-    <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart}>
+    <CartContext.Provider value={ctxValue}>
+      <Header/>
+      <Shop >
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
